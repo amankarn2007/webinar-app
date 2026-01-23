eae829a1c994d76637c1c433f1cc6255d785071a
Aman Karn <amankarn2007@gmail.com>
2026-01-23 09:16:23 +0530

    menu icon functionality

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,44 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
-function Sidebar(){
+function Sidebar({ isOpen, toggle }){
 
     return(
-        <div className="flex flex-col w-85 bg-white h-full hidden md:block z-1 shadow-xl">
-            {/*logo and img*/}
-            <div className="mt-3 px-6 flex items-center place-content-between">
+        isOpen ? 
+            <div className="flex flex-col w-85 bg-white h-full hidden md:block z-1 shadow-xl">
+    
+                {/*logo, menu closer and img*/}
+                <div className="mt-3 px-6 flex items-center place-content-between">
+    
+                    <Link to="/dashboard" className="text-md text-white w-34 flex bg-blue-700 h-10 justify-center items-center rounded-lg">
+    
+                        <span className='material-symbols-outlined text-green-200 pr-2 text-md!'>
+                            desktop_windows
+                        </span>
+                        Webinar<span className='text-green-400'>.gg</span>
+                    </Link>
+    
+                    <div className="bg-blue-50 p-1 rounded-xl hover:text-lg opacity-50 hover:opacity-100 cursor-pointer" onClick={toggle}>
+                        <i class="fa-solid fa-xmark"></i>
+                    </div>
 
-                <Link to="/dashboard" className="text-md text-white w-34 flex bg-blue-700 h-10 justify-center items-center rounded-lg">
-
-                    <span className='material-symbols-outlined text-green-200 pr-2 text-md!'>
-                        desktop_windows
-                    </span>
-                    Webinar<span className='text-green-400'>.gg</span>
-                </Link>
-
-                <div className="">
-                    <img src="../images/aman.png" alt="aman" className="h-14 w-13 rounded-xl"/>
+                    <div className="">
+                        <img src="../images/aman.png" alt="aman" className="h-14 w-13 rounded-xl"/>
+                    </div>
+                </div>
+                
+                <sideBarCloser />
+                
+                {/*menu*/}
+                <div className="px-5 mt-8 text-lg">
+                    <SideBarItems label={"Home"} icon="fa-house" />
+                    <SideBarItems label={"Webinars"} icon="fa-users" />
+                    <SideBarItems label={"Billing"} icon="fa-credit-card" />
+                    <SideBarItems label={"User Management"} icon="fa-users-gear" />
+                    <SideBarItems label={"Settings"} icon="fa-gear" />
                 </div>
             </div>
 
-            {/*menu*/}
-            <div className="px-5 mt-10 text-lg">
+        : <div></div>
+    )
+}
 
-                <div className="flex items-center place-content-between px-5 py-2 rounded-lg opacity-60 hover:bg-blue-50 hover:opacity-100">
-                    Home <i class="fa-solid fa-house"></i>
-                </div>
-                <div className="flex items-center place-content-between px-5 py-2 rounded-lg mt-2 opacity-60 hover:bg-blue-50 hover:opacity-100">
-                    Webinars <i class="fa-solid fa-users"></i>
-                </div>
-                <div className="flex items-center place-content-between px-5 py-2 rounded-lg mt-2 opacity-60 hover:bg-blue-50 hover:opacity-100">
-                    Billing <i class="fa-regular fa-credit-card"></i>
-                </div>
-                <div className="flex items-center place-content-between px-5 py-2 rounded-lg mt-2 opacity-60 hover:bg-blue-50 hover:opacity-100">
-                    User Management <i class="fa-solid fa-users-gear"></i>
-                </div>
-                <div className="flex items-center place-content-between px-5 py-2 rounded-lg mt-2 opacity-60 hover:bg-blue-50 hover:opacity-100">
-                    Settings <i class="fa-solid fa-gear"></i>
-                </div>
-            </div>
+//Reusing SideBarItems Component
+function SideBarItems({label, icon}) {
+    return(
+        <div className="flex items-center place-content-between px-5 py-2 rounded-lg mt-2 opacity-60 hover:bg-blue-50 hover:opacity-100">
+            { label } 
+            <i className={`fa-solid ${icon}`}></i>
         </div>
     )
 }
@@ -46,3 +56,4 @@ function Sidebar(){
 export default Sidebar;
 
 //before "md size" make screen "block", after that screen will be "default hidden"
+//hidden md:block   for responcive sidebar
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import '../App.css'
 import ActionCard from '../components/ActionCard';
 import CalenderView from '../components/CalenderView';
@@ -5,16 +6,29 @@ import Sidebar from '../components/Sidebar';
 import ProfileCard from '../components/UserProfileCard';
 
 function Dashboard() {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  function sidebarToggle() {
+    setSidebarOpen(!sidebarOpen);
+  }
+
   return(
     <div className='h-screen w-full bg-pink-200 flex'>
-      <Sidebar/>
+      <Sidebar isOpen={sidebarOpen} toggle={sidebarToggle} />
       
-      <main 
-        className='flex-1' //flex-1 = full width lelo
+      <main className='flex-1' //flex-1 = full width lelo
         style={{
           backgroundImage: "url('/images/cover.jpg')",
           backgroundSize: '100% 300px' // Image ko sirf top 300px tak rakha
         }} >
+
+          {/*agar sidebar open nahi hai tabhi menu icon dikhao*/}
+          { !sidebarOpen &&
+            <button className='fixed h-10 w-10 top-6 left-6 z-1 p-2 bg-white rounded-lg hover:bg-blue-50 cursor-pointer overflow-hidden' onClick={sidebarToggle}>
+              <i className="fa-solid fa-bars hover:text-lg"></i>
+            </button>
+          }
+
           <Main />
       </main>
     </div>
